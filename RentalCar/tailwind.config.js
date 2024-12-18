c2ebd6735ddc4ebb847d18b83464a4ee6d7507e9
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2024-12-18 12:17:51 +0100

    tailwind
    install tailwind css with node

---
+++ RentalCar/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+      './**/*.html', // Include all your HTML files
+      './Frontend/templates/**/*.html' // Adjust path for Django templates
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+}

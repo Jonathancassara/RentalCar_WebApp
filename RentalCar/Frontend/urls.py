1fd0b60def3b75287fd7aac2025d0f77dca9c4ac
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2024-12-18 15:16:58 +0100

    Update urls.py
    update
    import for view  ( list , add , update , delete)

--- RentalCar/Frontend/urls.py
+++ RentalCar/Frontend/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, list_rentals, add_rental, update_rental, delete_rental
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index, name='index'),  # Home Page
+    path('list/', list_rentals, name='list_rentals'),  # List Rentals
+    path('add/', add_rental, name='add_rental'),  # Add Rental
+    path('update/<int:pk>/', update_rental, name='update_rental'),  # Update Rental
+    path('delete/<int:pk>/', delete_rental, name='delete_rental'),  # Delete Rental
 ]

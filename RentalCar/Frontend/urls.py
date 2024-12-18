ecd7eb3b0d33c9ea79881ac2d00c4cff7d1e3628
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2024-12-18 19:12:48 +0100

    updare urls
    clean urls and comment
    for  easy read

--- RentalCar/Frontend/urls.py
+++ RentalCar/Frontend/urls.py
@@ -1,20 +1,31 @@
 from django.urls import path
-from .views import index, list_rentals, add_rental, update_rental, delete_rental, drivers,list_drivers, delete_drivers, add_driver, rentals, cars, add_car, list_cars, delete_cars
+from .views import (
+    index, 
+    list_rentals, add_rental, update_rental, delete_rental,
+    drivers, add_driver, list_drivers, delete_drivers, 
+    rentals, 
+    add_car, list_cars, delete_cars
+)
 
 urlpatterns = [
-    path('', index, name='index'),  # Home Page
-    path('list/', list_rentals, name='list_rentals'),  # List Rentals
-    path('add/', add_rental, name='add_rental'),  # Add Rental
-    path('update/<int:pk>/', update_rental, name='update_rental'),  # Update Rental
-    path('delete/<int:pk>/', delete_rental, name='delete_rental'),  # Delete Rental
-    path('drivers/', drivers, name='drivers'),# Drivers Page
-    path('drivers/add/', add_driver, name='add_driver'), #Add Drivers
-    path('list_drivers/', list_drivers, name='list_drivers'), #list Drivers
-    path('drivers/delete/', delete_drivers, name='delete_drivers'),  #delete drivers 
-    path('rentals/', rentals, name='rentals'),  # Rentals Page
-    #path('cars/', cars, name='cars'),          # Cars Page
-    path('cars/add/', add_car, name='add_car'),
-    path('cars/', list_cars, name='list_cars'),
-    path('cars/delete/', delete_cars, name='delete_cars'),
+    # Home Page
+    path('', index, name='index'),
 
+    # Rentals
+    path('rentals/', rentals, name='rentals'),
+    path('rentals/list/', list_rentals, name='list_rentals'),
+    path('rentals/add/', add_rental, name='add_rental'),
+    path('rentals/update/<int:pk>/', update_rental, name='update_rental'),
+    path('rentals/delete/<int:pk>/', delete_rental, name='delete_rental'),
+
+    # Drivers
+    path('drivers/', drivers, name='drivers'),
+    path('drivers/add/', add_driver, name='add_driver'),
+    path('drivers/list/', list_drivers, name='list_drivers'),
+    path('drivers/delete/', delete_drivers, name='delete_drivers'),
+
+    # Cars
+    path('cars/', list_cars, name='list_cars'),
+    path('cars/add/', add_car, name='add_car'),
+    path('cars/delete/', delete_cars, name='delete_cars'),
 ]
--- RentalCar/RentalCar/settings.py
+++ RentalCar/RentalCar/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Other apps
+    'django_extensions',
     #Frontend
     'Frontend',
 ]
